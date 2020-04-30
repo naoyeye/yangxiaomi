45d9b889e195cbc67b40da56de9d2e2dd1c7d218
naoyeye <naoyeye@gmail.com>
2020-04-30 14:13:22 +0800

    bump up version to 0.0.3

--- yangxiaomi/contentscript.js
+++ yangxiaomi/contentscript.js
@@ -2,12 +2,13 @@
 * @Author: hanjiyun
 * @Date:   2017-06-05 10:16:59
 * @Last Modified by:   hanjiyun
-* @Last Modified time: 2017-06-05 15:56:13
+* @Last Modified time: 2020-04-30 13:01:43
 */
 
 /* eslint-disable */
 
-var reg = /^https:\/\/www.douban.com\/(?:\?.+)?$/;
+var reg = /^https:\/\/(.+)douban.com\/(.+)?$/;
+
 if (reg.exec(window.location.href)) {
   $('.new-status.status-wrapper .status-real-wrapper').each(function(index, status) {
     var $s = $(status);
